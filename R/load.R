32b70c73bf25388524c4fa6f7b33fd1c122c11e5
Lena Mangold <lena@mangold.org>
2021-05-20 02:05:48 -0500

    update requirements_load examples

--- R/load.R
+++ R/load.R
@@ -19,13 +19,15 @@
 #' # The logic behind the requirements in the yaml file is:
 #' # - requirement 1: number of columns needs to be greater than 1
 #' # - requirement 2: number of columns where hdType is equal to Cat needs to be greater than 0
-#' req1 <- yaml::read_yaml("inst/test_dsvalidate/ex01-two-columns-one-categorical/dsvalidate/requirements.yaml")
+#' file_path <- system.file("test_dsvalidate", "ex01-two-columns-one-categorical","dsvalidate", package = "dsvalidate")
+#' req1 <- yaml::read_yaml(file.path(file_path, "requirements.yaml"))
 #' req1
 #'
 #' # Example 2: Need 2+ columns in total with fewer than 20 categories each
 #' # The logic behind the requirements in the yaml file is:
 #' # - number of columns where hdType is equal to Cat and n_distinct is less than 20 needs to be greater than 1
-#' req2 <- yaml::read_yaml("inst/test_dsvalidate/ex02-two-categorical-cols-max-distinct-cat/dsvalidate/requirements.yaml")
+#' file_path <- system.file("test_dsvalidate", "ex02-two-categorical-cols-max-distinct-cat","dsvalidate", package = "dsvalidate")
+#' req2 <- yaml::read_yaml(file.path(file_path, "requirements.yaml"))
 #' req2
 requirements_load <- function(path = "dsvalidate"){
 
