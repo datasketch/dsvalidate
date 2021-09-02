bdaa5a88d3a6b6e46c69b880f6ccc0398b372d40
Lena Mangold <lena@mangold.org>
2021-09-02 06:55:24 -0500

    write requirements_load fct

--- R/load.R
+++ R/load.R
@@ -4,31 +4,44 @@
 #' errors and warnings if necessary, and prepares it for use by
 #' validate_requirements.
 #'
-#' The list obtained from the requirements.yaml file is a list with a sublist
-#' for each requirement. Sublists (requirements are connected with logical AND).
-#'
-#' TODO: Would be good to enable AND and OR logic for requirements
-#'
 #' @param path Path for the requirements.yaml file; default is 'dsvalidate'
+#' @param filename Filename of requirements file, default is 'requirements.yaml'
 #'
 #' @return List of requirements
 #' @export
-#'
-#' @examples
-#' # Example 1: Need 2+ columns in total AND need at least one categorical column.
-#' # The logic behind the requirements in the yaml file is:
-#' # - requirement 1: number of columns needs to be greater than 1
-#' # - requirement 2: number of columns where hdType is equal to Cat needs to be greater than 0
-#' file_path <- system.file("test_dsvalidate", "ex01-two-columns-one-categorical","dsvalidate", package = "dsvalidate")
-#' req1 <- yaml::read_yaml(file.path(file_path, "requirements.yaml"))
-#' req1
-#'
-#' # Example 2: Need 2+ columns in total with fewer than 20 categories each
-#' # The logic behind the requirements in the yaml file is:
-#' # - number of columns where hdType is equal to Cat and n_distinct is less than 20 needs to be greater than 1
-#' file_path <- system.file("test_dsvalidate", "ex02-two-categorical-cols-max-distinct-cat","dsvalidate", package = "dsvalidate")
-#' req2 <- yaml::read_yaml(file.path(file_path, "requirements.yaml"))
-#' req2
-requirements_load <- function(path = "dsvalidate"){
+requirements_load <- function(path = "dsvalidate", filename = "requirements.yaml"){
 
+  if(!dir.exists(path)) stop("dsvalidate folder not found")
+
+  if(!grepl(".yaml", filename)) stop("Requirements file needs to be of type .yaml.")
+
+  file_path <- file.path(path, filename)
+  file_exists <- file.exists(file_path)
+
+  if(!file_exists) stop("Requirements file not found.")
+
+  requirements <- yaml::read_yaml(file_path)
+
+  if(length(requirements) == 0) stop("Requirements file empty.")
+
+  table_specs_exist <- "table" %in% names(requirements)
+  if(!table_specs_exist) stop("Requirements file doesn't contain table specs.")
+
+  table_names <- names(requirements$table)
+
+  table_specs_non_empty <- length(table_names) > 0
+  if(!table_specs_non_empty) stop("Requirements file doesn't contain table specs.")
+
+  table_names %>% purrr::map(function(.x){
+    if(any(!c("table_id", "specs", "fields") %in% names(requirements$table[[.x]]))) stop("All table requirements must include table_id, specs, and fields.")
+  })
+
+  requirements$table <- requirements$table %>% purrr::map(function(.x){
+    if(any(!c("table_id", "specs", "fields") %in% names(.x))) stop("All table requirements must include table_id, specs, and fields.")
+    if(!"n_cols" %in% names(.x$specs)) .x$specs$n_cols <- list(greater_than = 0)
+    if(!"n_rows" %in% names(.x$specs)) .x$specs$n_rows <- list(greater_than = 0)
+    .x
+  })
+
+  requirements
 }
