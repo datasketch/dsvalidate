31c207a30bdf7aab58f4fe6e103b41bbeaba4c6b
Lena Mangold <lena@mangold.org>
2021-09-02 08:44:59 -0500

    refactor validate fields fct

--- R/validate-fields.R
+++ R/validate-fields.R
@@ -13,49 +13,16 @@ validate_fields <- function(x, requirements){
     field_ids <- names(fields)
 
     # Initialise variables
-    columns_used <- c()
+    unavailable_columns <- c()
     output_table <- list()
 
-    fields_checked <- fields %>% purrr::map(function(.x){
-
-      field_id <- .x$field_id
-      id_required <- .x$id_required
-
-      validated_columns <- validate_columns(dic = dic, specs = .x$specs, field_id = field_id)
-
-      equals_required_id <- validated_columns$matches_id
-      id_found <- any(equals_required_id)
-
-      id_meets_requirements <- NULL
-
-      # Difference between number of columns that satisfy requirement and number of columns needed
-      diff_want_is <- NULL
-      if(id_required){
-        diff_want_is <- -1
-        if(id_found){
-          id_meets_requirements <- validated_columns[equals_required_id,]$meets_requirement
-          if(id_meets_requirements){
-            diff_want_is <- 0
-          }
-        }
-      } else {
-        specs_met <- validated_columns$meets_requirement
-        n_cols_meet_requirement <- sum(specs_met)
-
-        diff_want_is <- n_cols_meet_requirement - get_min_n_cols(req_n_cols = .x$n_cols)
-      }
-
-      .x$id_found <- id_found
-      .x$id_meets_requirements <- id_meets_requirements
-      .x$validated_columns <- validated_columns
-      .x$req_n_cols <- .x$n_cols
-      .x$diff_want_is <- diff_want_is
-      .x
-    }) %>% purrr::set_names(field_ids)
-
+    # Test all column in dic against all field specifications
+    field_requirements_checked <- fields %>%
+      purrr::map(~check_field_requirements(field = .x, dic = dic)) %>%
+      purrr::set_names(field_ids)
 
     # Order field specs so that loop starts with those where just enough columns satisfy requirement
-    fields_ordered <- order_fields(fields = fields_checked)
+    fields_ordered <- order_fields(fields = field_requirements_checked)
 
     for(field in fields_ordered){
 
@@ -63,47 +30,58 @@ validate_fields <- function(x, requirements){
       req_n_cols <- field$req_n_cols
 
       met <- FALSE
-      is_n_cols <- 0
+      n_columns_available <- 0
       use_cols <- NULL
 
       if(field$id_required){
         if(field$diff_want_is == 0){
           met <- field$id_meets_requirements
-          is_n_cols <- 1
+          n_columns_available <- 1
           use_cols <- field$field_id
         }
       } else {
         specs_met <- validated_columns$meets_requirement
-        cols_specs_met <- validated_columns[specs_met,]$id
 
         # check if columns already 'used' by other field spec
-        col_used_in_other_requirement <- intersect(cols_specs_met, columns_used)
-        col_not_used_in_other_requirement <- cols_specs_met[!cols_specs_met %in% col_used_in_other_requirement]
+        cols_specs_met <- validated_columns[specs_met,]$id
+        col_used_in_other_requirement <- intersect(cols_specs_met, unavailable_columns)
 
-        is_n_cols <- length(col_not_used_in_other_requirement)
+        if(length(col_used_in_other_requirement) == 0) {
+          col_used_in_other_requirement <- NULL
+          available_columns <- cols_specs_met
+        } else {
+          available_columns <- cols_specs_met[!cols_specs_met %in% col_used_in_other_requirement]
+        }
 
-        sufficient_n_cols <- eval_conditions(is_n_cols, names(req_n_cols), req_n_cols[[1]])
+        n_columns_available <- length(available_columns)
+
+        sufficient_n_cols <- eval_conditions(n_columns_available, names(req_n_cols), req_n_cols[[1]])
+
         if(sufficient_n_cols){
           met <- TRUE
           use_cols <- validated_columns[specs_met,]$id[1:(req_n_cols[[1]]+1)]
-          col_used_in_other_requirement <- NULL
-        } else {
-          if(length(col_used_in_other_requirement) == 0) {
-            col_used_in_other_requirement <- NULL
-          }
         }
       }
 
-      columns_used <- unique(c(columns_used, use_cols))
+      # Add use_cols to unavailable_columns so in next iteration, the columns used in this iteration will not be available
+      unavailable_columns <- unique(c(unavailable_columns, use_cols))
 
-      output_table[[field$field_id]] <- list(met = met,
-                                             id_found = field$id_found,
-                                             id_required = field$id_required,
-                                             specs = field$specs,
-                                             req_n_cols = req_n_cols,
-                                             is_n_cols = is_n_cols,
-                                             use_cols = use_cols,
-                                             col_used_in_other_requirement = col_used_in_other_requirement)
+      if(met){
+        field_validation <- list(met = TRUE,
+                                 use_cols = use_cols)
+      } else {
+
+        field_validation <- list(met = FALSE,
+                                 id_found = field$id_found,
+                                 id_required = field$id_required,
+                                 specs = field$specs,
+                                 req_n_cols = req_n_cols,
+                                 n_columns_available = n_columns_available,
+                                 use_cols = use_cols,
+                                 col_used_in_other_requirement = col_used_in_other_requirement)
+      }
+
+      output_table[[field$field_id]] <- field_validation
 
     }
 
@@ -161,7 +139,8 @@ validate_columns <- function(dic, specs, field_id){
     dplyr::rowwise() %>%
     dplyr::mutate(meets_requirement = all(dplyr::c_across(!id))) %>%
     dplyr::select(id, meets_requirement) %>%
-    dplyr::mutate(matches_id = id == field_id)
+    dplyr::mutate(matches_id = id == field_id) %>%
+    dplyr::ungroup()
 }
 
 
@@ -174,4 +153,45 @@ order_fields <- function(fields){
     dplyr::pull(name)
 
   fields[order]
+}
+
+
+check_field_requirements <- function(field, dic){
+
+  field_id <- field$field_id
+  id_required <- field$id_required
+
+  validated_columns <- validate_columns(dic = dic, specs = field$specs, field_id = field_id)
+
+  equals_required_id <- validated_columns$matches_id
+  id_found <- any(equals_required_id)
+
+  id_meets_requirements <- validated_columns[equals_required_id,]$meets_requirement
+
+  # Difference between number of columns that satisfy requirement and number of columns needed
+  diff_want_is <- get_diff_want_is(id_required, id_found, id_meets_requirements, validated_columns, field)
+
+  field$id_found <- id_found
+  field$id_meets_requirements <- id_meets_requirements
+  field$validated_columns <- validated_columns
+  field$req_n_cols <- field$n_cols
+  field$diff_want_is <- diff_want_is
+  field
+}
+
+
+get_diff_want_is <- function(id_required, id_found, id_meets_requirements, validated_columns, field){
+  diff_want_is <- NULL
+  if(id_required){
+    diff_want_is <- -1
+    if(id_found & id_meets_requirements){
+      diff_want_is <- 0
+    }
+  } else {
+    specs_met <- validated_columns$meets_requirement
+    n_cols_meet_requirement <- sum(specs_met)
+
+    diff_want_is <- n_cols_meet_requirement - get_min_n_cols(req_n_cols = field$n_cols)
+  }
+  diff_want_is
 }
