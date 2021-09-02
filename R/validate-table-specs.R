27a9f824ae13ff9a88925f95c48e4468f57a4869
Lena Mangold <lena@mangold.org>
2021-09-02 06:57:30 -0500

    improve table specs fcts

--- R/validate-table-specs.R
+++ R/validate-table-specs.R
@@ -10,17 +10,59 @@ validate_table_specs <- function(x, requirements){
 
     output[[table_id]]$n_cols <- validate_n_cols(table = table, specs_n_cols = specs$n_cols)
 
-    output[[table_id]]$n_rows <- validate_n_rows(table = table, specs_n_rows = specs$n_rows)
+    output[[table_id]]$n_rows <- validate_n_rows(table = table, specs_n_rows = specs$n_rows,
+                                                 validated_n_cols = output[[table_id]]$n_cols$met)
 
-    output[[table_id]]$frType <- validate_frType(table = table, specs_frType = specs$frType)
+    output[[table_id]]$frType <- validate_frType(table = table, specs_frType = specs$frType,
+                                                 validated_n_cols = output[[table_id]]$n_cols$met,
+                                                 validated_n_rows = output[[table_id]]$n_rows$met)
   }
 
   output
 }
 
 
-validate_frType <- function(table, specs_frType){
-  if(is.null(specs_frType)) return()
+validate_n_cols <- function(table, specs_n_cols){
+
+  n_cols_actual <- ncol(table)
+
+  met <- eval_conditions(n_cols_actual, names(specs_n_cols), specs_n_cols[[1]])
+
+  n_cols <- validated()
+
+  if(!met){
+    n_cols <- not_validated(want = specs_n_cols,
+                            is = n_cols_actual)
+  }
+
+  n_cols
+}
+
+
+validate_n_rows <- function(table, specs_n_rows, validated_n_cols = TRUE){
+
+  if(!validated_n_cols) return(not_validated(want = specs_n_rows,
+                                             is = 0))
+
+  n_rows_actual <- nrow(table)
+
+  met <- eval_conditions(n_rows_actual, names(specs_n_rows), specs_n_rows[[1]])
+
+  n_rows <- validated()
+
+  if(!met){
+    n_rows <- not_validated(want = specs_n_rows,
+                            is = n_rows_actual)
+  }
+
+  n_rows
+}
+
+
+validate_frType <- function(table, specs_frType, validated_n_cols = TRUE, validated_n_rows = TRUE){
+
+  if(!validated_n_cols | !validated_n_rows) return(not_validated(want = specs_frType,
+                                                                 is = "none"))
 
   fringe_table <- homodatum::fringe(table)
   frType_actual_string <- as.character(fringe_table$frtype)
@@ -29,57 +71,15 @@ validate_frType <- function(table, specs_frType){
   condition <- names(specs_frType)
   frType_expected <- unlist(strsplit(specs_frType[[condition]], split='-', fixed=TRUE))
 
-  met <- eval_conditions(frType_expected, condition, frType_actual)
+  met <- eval_conditions(frType_actual, condition, frType_expected)
 
-  frType <- list(met = TRUE)
+  frType <- validated()
 
   if(!met){
-    frType <- list(met = FALSE,
-                   want = specs_frType,
-                   is = frType_actual_string)
+    frType <- not_validated(want = specs_frType,
+                            is = frType_actual_string)
   }
 
   frType
 }
-
-
-
-validate_n_cols <- function(table, specs_n_cols){
-  if(is.null(specs_n_cols)) return()
-
-  n_cols_actual <- ncol(table)
-
-  met <- eval_conditions(n_cols_actual, names(specs_n_cols), specs_n_cols[[1]])
-
-  n_cols <- list(met = TRUE)
-
-  if(!met){
-    n_cols <- list(met = FALSE,
-                   want = specs_n_cols,
-                   is = n_cols_actual)
-  }
-
-  n_cols
-}
-
-
-validate_n_rows <- function(table, specs_n_rows){
-  if(is.null(specs_n_rows)) return()
-
-  n_rows_actual <- nrow(table)
-
-  met <- eval_conditions(n_rows_actual, names(specs_n_rows), specs_n_rows[[1]])
-
-  n_rows <- list(met = TRUE)
-
-  if(!met){
-    n_rows <- list(met = FALSE,
-                   want = specs_n_rows,
-                   is = n_rows_actual)
-  }
-
-  n_rows
-}
-
-
 
