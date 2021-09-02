8b2d4f293a0f8f50640d60e13a477428fd285dff
Lena Mangold <lena@mangold.org>
2021-09-02 06:58:28 -0500

    first working version of validate fields

--- R/validate-fields.R
+++ R/validate-fields.R
@@ -8,125 +8,84 @@ validate_fields <- function(x, requirements){
     table <- x[[table_id]]
     dic <- create_dic(table, extended = TRUE)
 
-    fields <- requirements$table[[table_id]]$fields
-
-    # If n_cols greater than 0 has been left out as short-cut it is added here
-    fields <- fields %>% purrr::map(function(.x){
-      if(is.null(.x$n_cols)) .x$n_cols <- list(greater_than = 0)
-      if(is.null(.x$id_required)) .x$id_required <- FALSE
-      .x
-    })
+    fields <- check_fields(fields = requirements$table[[table_id]]$fields)
 
     field_ids <- names(fields)
 
+    # Initialise variables
     columns_used <- c()
-
     output_table <- list()
 
-    fields_checked <- field_ids %>% purrr::map(function(field_id){
+    fields_checked <- fields %>% purrr::map(function(.x){
 
-      field <- fields[[field_id]]
-      id_required <- field$id_required
-      want_n_cols <- field$n_cols
-      if(!is.list(want_n_cols)) want_n_cols <- list(equals = want_n_cols)
+      field_id <- .x$field_id
+      id_required <- .x$id_required
 
-      n_cols_needed <- want_n_cols[[1]]
-      if(names(want_n_cols) == "greater_than") n_cols_needed <- n_cols_needed + 1
+      validated_columns <- validate_columns(dic = dic, specs = .x$specs, field_id = field_id)
 
-      specs <- field$specs
-
-      # If `equals` has been left out as short-cut it is added here
-      for(specs_type in names(specs)){
-        specification <- specs[[specs_type]]
-        if(!is.list(specification)) specs[[specs_type]] <- list(equals = specification)
-      }
-
-      dic_selected <- dic %>% dplyr::select(id, dplyr::any_of(names(specs)) & !label)
-
-      field_validated <- dic_selected %>%
-        dplyr::mutate(dplyr::across(!id,  ~ eval(., specs, dplyr::cur_column()))) %>%
-        dplyr::rowwise() %>%
-        dplyr::mutate(meets_requirement = all(dplyr::c_across(!id))) %>%
-        dplyr::select(id, meets_requirement) %>%
-        dplyr::mutate(matches_id = id == field_id)
-
-      use_cols <- NULL
-      col_used_in_other_requirement <- NULL
-
-      equals_required_id <- field_validated$matches_id
+      equals_required_id <- validated_columns$matches_id
       id_found <- any(equals_required_id)
+
+      id_meets_requirements <- NULL
 
       # Difference between number of columns that satisfy requirement and number of columns needed
       diff_want_is <- NULL
       if(id_required){
         diff_want_is <- -1
         if(id_found){
-          id_meets_requirements <- field_validated[equals_required_id,]$meets_requirement
+          id_meets_requirements <- validated_columns[equals_required_id,]$meets_requirement
           if(id_meets_requirements){
             diff_want_is <- 0
           }
         }
       } else {
-        specs_met <- field_validated$meets_requirement
+        specs_met <- validated_columns$meets_requirement
         n_cols_meet_requirement <- sum(specs_met)
 
-        diff_want_is <- n_cols_meet_requirement - n_cols_needed
+        diff_want_is <- n_cols_meet_requirement - get_min_n_cols(req_n_cols = .x$n_cols)
       }
 
-      field$id_found <- id_found
-      field$field_validated <- field_validated
-      field$diff_want_is <- diff_want_is
-      field
+      .x$id_found <- id_found
+      .x$id_meets_requirements <- id_meets_requirements
+      .x$validated_columns <- validated_columns
+      .x$req_n_cols <- .x$n_cols
+      .x$diff_want_is <- diff_want_is
+      .x
     }) %>% purrr::set_names(field_ids)
 
 
     # Order field specs so that loop starts with those where just enough columns satisfy requirement
-    field_order <- fields_checked %>%
-      purrr::map_df("diff_want_is") %>%
-      tidyr::pivot_longer(cols = everything()) %>%
-      dplyr::arrange(value) %>%
-      dplyr::select(-value) %>%
-      dplyr::pull(name)
-
-    fields_ordered <- fields_checked[field_order]
+    fields_ordered <- order_fields(fields = fields_checked)
 
     for(field in fields_ordered){
 
-      field_id <- field$field_id
-      id_required <- field$id_required
-      field_validated <- field$field_validated
-      want_n_cols <- field$n_cols
-      specs <- field$specs
-
-      equals_required_id <- field_validated$matches_id
-      id_found <- any(equals_required_id)
+      validated_columns <- field$validated_columns
+      req_n_cols <- field$req_n_cols
 
       met <- FALSE
       is_n_cols <- 0
       use_cols <- NULL
-      diff_want_is <- NULL
-      if(id_required){
-        diff_want_is <- -1
-        if(id_found){
-          id_meets_requirements <- field_validated[equals_required_id,]$meets_requirement
-          met <- id_meets_requirements
+
+      if(field$id_required){
+        if(field$diff_want_is == 0){
+          met <- field$id_meets_requirements
           is_n_cols <- 1
-          use_cols <- field_id
-          diff_want_is <- 0
+          use_cols <- field$field_id
         }
       } else {
-        specs_met <- field_validated$meets_requirement
-        cols_specs_met <- field_validated[specs_met,]$id
+        specs_met <- validated_columns$meets_requirement
+        cols_specs_met <- validated_columns[specs_met,]$id
 
+        # check if columns already 'used' by other field spec
         col_used_in_other_requirement <- intersect(cols_specs_met, columns_used)
         col_not_used_in_other_requirement <- cols_specs_met[!cols_specs_met %in% col_used_in_other_requirement]
 
         is_n_cols <- length(col_not_used_in_other_requirement)
 
-        sufficient_n_cols <- eval_conditions(is_n_cols, names(want_n_cols), want_n_cols[[1]])
+        sufficient_n_cols <- eval_conditions(is_n_cols, names(req_n_cols), req_n_cols[[1]])
         if(sufficient_n_cols){
           met <- TRUE
-          use_cols <- field_validated[specs_met,]$id[1:(want_n_cols[[1]]+1)]
+          use_cols <- validated_columns[specs_met,]$id[1:(req_n_cols[[1]]+1)]
           col_used_in_other_requirement <- NULL
         } else {
           if(length(col_used_in_other_requirement) == 0) {
@@ -137,14 +96,14 @@ validate_fields <- function(x, requirements){
 
       columns_used <- unique(c(columns_used, use_cols))
 
-      output_table[[field_id]] <- list(met = met,
-                                       id_found = id_found,
-                                       id_required = id_required,
-                                       specs = specs,
-                                       want_n_cols = want_n_cols,
-                                       is_n_cols = is_n_cols,
-                                       use_cols = use_cols,
-                                       col_used_in_other_requirement = col_used_in_other_requirement)
+      output_table[[field$field_id]] <- list(met = met,
+                                             id_found = field$id_found,
+                                             id_required = field$id_required,
+                                             specs = field$specs,
+                                             req_n_cols = req_n_cols,
+                                             is_n_cols = is_n_cols,
+                                             use_cols = use_cols,
+                                             col_used_in_other_requirement = col_used_in_other_requirement)
 
     }
 
@@ -154,3 +113,65 @@ validate_fields <- function(x, requirements){
   output
 }
 
+
+check_fields <- function(fields){
+  fields %>% purrr::map(function(.x){
+    .x$n_cols <- check_req_n_cols(n_cols = .x$n_cols)
+    .x$specs <- check_specs(specs = .x$specs)
+    if(is.null(.x$id_required)) .x$id_required <- FALSE
+    .x
+  })
+}
+
+
+check_specs <- function(specs){
+  if(is.null(specs)) stop("Column specs missing.")
+  # If `equals` has been left out as short-cut it is added here
+  for(specs_type in names(specs)){
+    specification <- specs[[specs_type]]
+    if(!is.list(specification)) specs[[specs_type]] <- list(equals = specification)
+  }
+
+  specs
+}
+
+
+check_req_n_cols <- function(n_cols){
+  # If n_cols is null, must be greater than 0
+  if(is.null(n_cols)) n_cols <- list(greater_than = 0)
+  # If n_cols greater than 0 has been left out as short-cut it is added here
+  if(!is.list(n_cols)) n_cols <- list(equals = n_cols)
+  n_cols
+}
+
+
+get_min_n_cols <- function(req_n_cols){
+  n_cols <- req_n_cols[[1]]
+  if(names(req_n_cols) == "greater_than") n_cols <- n_cols + 1
+  n_cols
+}
+
+
+validate_columns <- function(dic, specs, field_id){
+  dic_selected <- dic %>%
+    dplyr::select(id, dplyr::any_of(names(specs)) & !label)
+
+  dic_selected %>%
+    dplyr::mutate(dplyr::across(!id,  ~ eval(., specs, dplyr::cur_column()))) %>%
+    dplyr::rowwise() %>%
+    dplyr::mutate(meets_requirement = all(dplyr::c_across(!id))) %>%
+    dplyr::select(id, meets_requirement) %>%
+    dplyr::mutate(matches_id = id == field_id)
+}
+
+
+order_fields <- function(fields){
+  order <- fields %>%
+    purrr::map_df("diff_want_is") %>%
+    tidyr::pivot_longer(cols = everything()) %>%
+    dplyr::arrange(value) %>%
+    dplyr::select(-value) %>%
+    dplyr::pull(name)
+
+  fields[order]
+}
