65a7c23be3b3b437e467414eaa7fce368001027f
Lena Mangold <lena@mangold.org>
2021-09-02 06:57:14 -0500

    improve table meta fcts

--- R/validate-table-meta.R
+++ R/validate-table-meta.R
@@ -9,12 +9,14 @@ validate_table_meta <- function(x, requirements){
                                          n_tables_expected = n_tables_expected)
 
   output$n_tables <- validate_n_tables(x = x,
-                                       n_tables_expected = n_tables_expected)
+                                       n_tables_expected = n_tables_expected,
+                                       validated_data_type = output$data_type$met)
 
   output$table_ids <- validate_table_ids(x = x,
                                          n_tables_expected = n_tables_expected,
                                          table_ids_expected = table_ids_expected,
-                                         data_type_met = output$data_type$met)
+                                         validated_data_type = output$data_type$met,
+                                         validated_n_tables = output$n_tables$met)
 
   output
 }
@@ -22,75 +24,96 @@ validate_table_meta <- function(x, requirements){
 
 validate_data_type <- function(x, n_tables_expected){
   if(n_tables_expected > 1){
+    want <- "list of dataframes"
     if(is.data.frame(x)){
-      data_type <- list(met = FALSE,
-                        want = "list of dataframes",
-                        is = "data.frame")
+      data_type <- not_validated(want = want,
+                                 is = "data.frame")
     } else if (is.list(x)) {
-      data_type <- list(met = TRUE)
+      is_type_dataframe <- x %>% purrr::map(~ is.data.frame(.x)) %>% unlist()
+      if(all(is_type_dataframe)){
+        data_type <- validated()
+      } else {
+        data_type <- not_validated(want = want,
+                                   is = "list contains non-dataframes")
+      }
+
     } else {
-      data_type <- list(met = FALSE,
-                        want = "list of dataframes",
-                        is = class(x))
+      data_type <- not_validated(want = want,
+                                 is = class(x))
     }
   } else {
+    want <- "data.frame"
     if(is.data.frame(x)){
-      data_type <- list(met = TRUE)
+      data_type <- validated()
+    } else if (is.list(x) & is.data.frame(x[[1]])){
+      data_type <- validated()
+    } else if (is.list(x)){
+      data_type <- not_validated(want = want,
+                                 is = paste0("list of ", class(x[[1]])))
     } else {
-      data_type <- list(met = FALSE,
-                        want = "list of dataframes",
-                        is = class(x))
+      data_type <- not_validated(want = want,
+                                 is = class(x))
     }
   }
   data_type
 }
 
 
-validate_n_tables <- function(x, n_tables_expected){
+validate_n_tables <- function(x, n_tables_expected, validated_data_type = TRUE){
   n_tables <- NULL
+
+  if(!validated_data_type) return(not_validated(want = n_tables_expected,
+                                                is = "none"))
+
   if(n_tables_expected > 1){
     if(is.data.frame(x)){
-      n_tables <- list(met = FALSE,
-                       want = n_tables_expected,
-                       is = 1)
+      n_tables <- not_validated(want = n_tables_expected,
+                                is = 1)
     } else if (is.list(x)) {
       n_tables_actual <- length(x)
       met <- FALSE
       if(n_tables_actual == n_tables_expected | n_tables_actual > n_tables_expected) met <- TRUE
-      n_tables <- list(met = TRUE)
+      n_tables <- validated()
       if(!met){
-        n_tables <- list(met = met,
-                         want = n_tables_expected,
-                         is = n_tables_actual)
+        n_tables <- not_validated(want = n_tables_expected,
+                                  is = n_tables_actual)
       }
     }
   } else {
     if(is.data.frame(x)){
-      n_tables <- list(met = TRUE)
+      n_tables <- validated()
+    } else if(is.list(x)){
+      if(length(x) == 1){
+        n_tables <- validated()
+      } else {
+        n_tables <- not_validated(want = 1,
+                                  is = length(x))
+      }
     }
   }
+
   n_tables
 }
 
-validate_table_ids <- function(x, n_tables_expected, table_ids_expected, data_type_met){
+validate_table_ids <- function(x, n_tables_expected, table_ids_expected, validated_data_type = TRUE, validated_n_tables = TRUE){
   table_ids <- NULL
   if(n_tables_expected > 1){
-    table_ids_actual <- names(x)
-    table_ids <- list(met = TRUE)
 
-    if(!data_type_met){
-      table_ids <- list(met = FALSE,
-                        want = table_ids_expected,
-                        is = "none")
-    } else if(is.null(table_ids_actual)){
-      table_ids <- list(met = FALSE,
-                        want = table_ids_expected,
-                        is = "unnamed tables")
+    if(!validated_data_type | !validated_n_tables) return(not_validated(want = table_ids_expected,
+                                                                        is = "none"))
+
+    table_ids_actual <- names(x)
+    table_ids <- validated()
+
+    if(is.null(table_ids_actual)){
+      table_ids <- not_validated(want = table_ids_expected,
+                                 is = "unnamed tables")
     } else if (!all(table_ids_expected %in% table_ids_actual)){
-      table_ids <- list(met = FALSE,
-                        want = table_ids_expected,
-                        is = table_ids_actual)
+      table_ids <- not_validated(want = table_ids_expected,
+                                 is = table_ids_actual)
     }
+
+
   }
   table_ids
 }
